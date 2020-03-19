b3b9beb5ac3d4c3cb8e5adefc7efa30dccb8da6e
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-03-19 16:57:49 -0400

    finished part of task 3

--- index.js
+++ index.js
@@ -108,10 +108,10 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-  constructor(person) {
-    this.name = person.name;
-    this.age = person.age;
-    this.location = person.location;
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
   }
   speak(){
     return `Hello my name is ${this.name}, I am from ${this.location}.`
@@ -132,8 +132,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(attributes) {
+    super(attributes)
+      this.specialty = attributes.specialty;
+      this.favLanguage = attributes.favLanguage;
+      this.catchPhrase = attributes.catchPhrase;
+  }
+  demo() {
+    return `Today we are learning about ${subject}`;
+  }
+  grade() {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
